4baad66e0fab7ed68037413e80e3c3b6ef601035
emily080913 <cardenasemilytatiana@gmail.com>
2025-10-21 08:50:16 -0500

    Proyecto Base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace proyecto_caldas.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
